1e8552bd7e4a815f499db57429fb917f86a56f63
Inkflow59 <tomcucherosset@hotmail.fr>
2025-03-13 09:30:05 +0100

    Modification du fichier module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module fr.marieteam.pdf.marieteampdf {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
     requires java.sql;
-
 
     opens fr.marieteam.pdf.marieteampdf to javafx.fxml;
     exports fr.marieteam.pdf.marieteampdf;
